4b62f239f07ba7051f7efa8994927f2ea3f4e1bc
BeLieVer031 <sandyrajak031@gmail.com>
2023-12-30 22:11:28 +0530

    UserModel created

--- backend/models/UserModel.ts
+++ backend/models/UserModel.ts
@@ -1,5 +1,12 @@
 import { Schema, model } from "mongoose";
 import { IRegisterUser } from "../types";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+interface IPayload {
+    _id: string
+    email: string
+    username?: string
+}
 
 const userSchema = new Schema<IRegisterUser>({
     username: {
@@ -39,8 +46,40 @@ const userSchema = new Schema<IRegisterUser>({
     timestamps: true
 })
 
-userSchema.pre("save", function () {
+// password hashing before saving the actual document
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return;
+    const SALT = await bcrypt.genSalt(+process.env.SALT_ROUND!)
+    this.password = await bcrypt.hash(this.password, SALT)
+    next()
 })
+
+// function for comparing the password 
+userSchema.methods.comparePassword = async function (password: string): Promise<boolean> {
+    const isPassword = await bcrypt.compare(password, this.password)
+    return isPassword;
+}
+
+// function for genrating the access_token 
+userSchema.methods.generateAccessToken = function () {
+    const payload: IPayload = {
+        _id: this._id,
+        email: this.email,
+        username: this.username
+    }
+    const accessToken = jwt.sign(payload, process.env.ACCESS_SECRET_TOKEN!)
+    return accessToken;
+}
+
+// function for generating the refresh_token 
+userSchema.methods.generateRefreshToken = function () {
+    const payload: IPayload = {
+        _id: this._id,
+        email: this.email,
+    }
+    const refreshToken = jwt.sign(payload, process.env.REFRESH_SECRET_TOKEN!)
+    return refreshToken;
+}
 
 const UserModel = model<IRegisterUser>("Users", userSchema);
 
